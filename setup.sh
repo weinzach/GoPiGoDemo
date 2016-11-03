4ac980d45c9e9f1cf14be5dd5e46cf838376e159
Zachary Weingarten <zdog1230@yahoo.com>
2016-11-02 20:17:15 -0400

    Initial Commit

---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "Intiailzing..."
+
+# Verify that some packages are installed
+requirepackage(){
+   if [ ! -z "$2" ] 
+      then
+         ldconfig -p | grep $2 > /dev/null
+      else
+         which $1 > /dev/null
+   fi
+   if [ $? != 0 ]
+      then
+         echo "Package $1: Installing..."
+         apt-get install --assume-yes $1 > /dev/null
+         echo "Package $1: Complete."
+      else
+         echo "Package $1: Already installed."
+   fi
+}
+ 
+#Install Required Packages  
+requirepackage git
+requirepackage curl
+sudo apt-get install -y build-essential
+
+#Grab Latest Version of NodeJS
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+#Get GoPiGo Example Code
+git clone https://github.com/DexterInd/GoPiGo
+cd $PWD/GoPiGo/Setup
+#Install Requirements
+sudo bash install.sh
+
+#Install Demo Dependencies
+
+cd $PWD/GoPiGoWebClient
+npm install
+
+cd $PWD/GoPiGoWebServer
+npm install
+
+echo "Setup Complete!"
